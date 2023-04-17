fc8d9db96fcc2c942ce2f8770d5217c30577e3a6
Giovani <giovani.o@outlook.com.br>
2023-04-17 19:18:15 -0300

    style: Formulário de comentários

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -32,6 +32,18 @@ export function Post() {
           <a href="#">#React</a>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea 
+          placeholder="Deixe um comentário"
+        />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   )
 }
