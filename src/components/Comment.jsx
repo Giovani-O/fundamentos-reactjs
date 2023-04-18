2811c48f0819f84e1bed2e919665398855fb249c
Giovani <giovani.o@outlook.com.br>
2023-04-18 13:32:06 -0300

    feat: Front-end finalizado

---
+++ src/components/Comment.jsx
@@ -0,0 +1,40 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/giovani-o.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Giovani de Oliveira</strong>
+
+              <time 
+                title="11 de Maio às 11:13" 
+                dateTime="2022-05-11 08:13:30"
+              >
+                1h atrás
+              </time>
+            </div>
+
+            <button title="Excluir comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Lorem ipsum dolor sit amet!</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
