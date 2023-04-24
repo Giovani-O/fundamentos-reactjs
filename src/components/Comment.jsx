b4202db1e8b18b94556eacc7ec08fe8c8aa41e98
Giovani <giovani.o@outlook.com.br>
2023-04-24 13:21:27 -0300

    feat: insere comentários declarativamente

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,7 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
 
-export function Comment() {
+export function Comment({ content }) {
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/giovani-o.png" />
@@ -26,7 +26,7 @@ export function Comment() {
             </button>
           </header>
 
-          <p>Lorem ipsum dolor sit amet!</p>
+          <p>{ content }</p>
         </div>
 
         <footer>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -8,7 +8,11 @@ import { Comment } from './Comment';
 import styles from './Post.module.css';
 
 export function Post({ author, publishedAt, content }) {
-  const [ comments, setComments ] = useState( [1, 2] )
+  const [ comments, setComments ] = useState( [
+    'Lorem ipsum dolor sit amet',
+  ] );
+
+  const [newCommentText, setNewCommentText] = useState('');
 
   const publicationDateFormatted = format(
     publishedAt, "d 'de' LLLL 'às' HH:mm'h'", { locale: ptBR }
@@ -21,8 +25,13 @@ export function Post({ author, publishedAt, content }) {
   function handleCreateNewComment() {
     event.preventDefault();
 
-    setComments([...comments, comments.length + 1])
+    setComments([...comments, newCommentText]);
+    setNewCommentText('');
   };
+
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value);
+  }
 
   return (
     <article className={ styles.post }>
@@ -55,7 +64,10 @@ export function Post({ author, publishedAt, content }) {
         <strong>Deixe seu feedback</strong>
 
         <textarea 
+          name="comment"
           placeholder="Deixe um comentário"
+          value={newCommentText}
+          onChange={handleNewCommentChange}
         />
 
         <footer>
@@ -65,7 +77,7 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         { comments.map(comment => {
-          return <Comment />
+          return <Comment key={comments.indexOf(comment)} content={comment}/>
         }) }
       </div>
     </article>
