d229051d9bede58954ee5a2d8bf86a15caea77b9
Giovani <giovani.o@outlook.com.br>
2023-05-02 13:00:59 -0300

    feat: exclusão de commentários

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,11 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+  
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/giovani-o.png" />
@@ -21,7 +25,7 @@ export function Comment({ content }) {
               </time>
             </div>
 
-            <button title="Excluir comentário">
+            <button onClick={handleDeleteComment} title="Excluir comentário">
               <Trash size={24} />
             </button>
           </header>
