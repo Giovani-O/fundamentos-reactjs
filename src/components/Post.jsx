7e4c085270070cd5ac5a6428ae9db69a471c7be1
Giovani <giovani.o@outlook.com.br>
2023-05-02 12:09:15 -0300

    fix: keys

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -52,10 +52,10 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.content}>
         { content.map(line => {
           if (line.type === 'paragraph') {
-            return <p key={content.indexOf(line)}>{line.content}</p>
+            return <p key={line.content}>{line.content}</p>
           }
           else if (line.type === 'link') {
-            return <p key={content.indexOf(line)}><a href="#">{line.content}</a></p>
+            return <p key={line.content}><a href="#">{line.content}</a></p>
           }
         })}
       </div>
@@ -77,7 +77,7 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         { comments.map(comment => {
-          return <Comment key={comments.indexOf(comment)} content={comment}/>
+          return <Comment key={comment} content={comment}/>
         }) }
       </div>
     </article>
